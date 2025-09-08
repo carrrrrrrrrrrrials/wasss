de105840b99d0bd894e453b469099e2b6a56a197
carrrrrrrrrrrrials <carrialsxd@gmail.com>
2025-09-08 21:31:07 +0300

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -1,3 +1,4 @@
 print("Loader running.")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/grandmaesx0-cell/loader/refs/heads/main/configLoader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/grandmaesx0-cell/loader/refs/heads/main/scriptLoader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/carrrrrrrrrrrrials/wasss/refs/heads/main/configLoader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/carrrrrrrrrrrrials/wasss/refs/heads/main/scriptLoader.lua"))()
+
